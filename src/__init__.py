7a4f4d3c639b12d934e102af5d9203b32cb8af08
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2023-02-02 15:41:16 +0800

    temporarily allow any cross-origin requests

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 
 from src.common import auth
@@ -10,4 +11,5 @@ app.add_event_handler("startup", connect)
 app.add_event_handler("shutdown", close)
 app.include_router(GraphQLRouter(schema, context_getter=auth.get_context), prefix="/graphql", tags=["GraphQL"])
 app.include_router(auth.router)
+app.add_middleware(CORSMiddleware, allow_origins="*", allow_credentials=True, allow_methods="*", allow_headers="*")
 app.middleware("http")(retry_when_lose_sql_connection)
